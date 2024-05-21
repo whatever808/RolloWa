14f4f9b19d1414cc68837042e638b5bfcb261a1f
leeyechanbal <dpcks0983@gmail.com>
2024-05-21 14:23:35 +0900

    merge
    merge

--- src/main/java/com/br/project/controller/pay/VacationController.java
+++ src/main/java/com/br/project/controller/pay/VacationController.java
@@ -14,7 +14,12 @@ import lombok.RequiredArgsConstructor;
 public class VacationController {
 	private final VacationService vacaService;
 	
-	@GetMapping("/selectVacation.page")
+	/**
+	 * 휴가 조회 및 신청을 할 수 있는 페이지
+	 * @author 에찬
+	 * @return 휴가 카테고리 와 휴가 현황을 반환 
+	 */
+	@GetMapping("/vacation.page")
 	public void selectVacation() {
 		
 	}
