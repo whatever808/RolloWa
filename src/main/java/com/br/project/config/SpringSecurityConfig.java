d72fac6f4ff4092b80b52ec5b6c2fe9094dca441
Bae Ki Woong <12171790@inha.edu>
2024-05-30 20:59:34 +0900

    update: spring security 구현중

--- src/main/java/com/br/project/config/SpringSecurityConfig.java
+++ src/main/java/com/br/project/config/SpringSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.br.project.config;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,13 +10,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
-
+	@Autowired
+	private HttpSession session;
+	String contextPath = session.getServletContext().getContextPath();
+	
+	
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {    	
         http
 			.authorizeHttpRequests()
-	        .mvcMatchers("/admin").hasRole("ADMIN")
+	        .mvcMatchers(contextPath).hasRole("ADMIN")
 	        .anyRequest().authenticated();
 
         return http.build();
