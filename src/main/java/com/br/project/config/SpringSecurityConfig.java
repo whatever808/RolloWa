1427b786228122955fa88659f21c6565cd212d9c
Bae Ki Woong <12171790@inha.edu>
2024-06-04 13:52:34 +0900

    etc: spring security 구현중

--- src/main/java/com/br/project/config/SpringSecurityConfig.java
+++ src/main/java/com/br/project/config/SpringSecurityConfig.java
@@ -6,22 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
-	/*@Autowired
-	private HttpSession session;
-	String contextPath = session.getServletContext().getContextPath();
-	
-	
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {    	
-        http
-			.authorizeHttpRequests()
-	        .mvcMatchers(contextPath).hasRole("ADMIN")
-	        .anyRequest().authenticated();
 
-        return http.build();
-    }*/
+	/*@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.regexMatchers("/", "main").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.permitAll()
+			)
+			.logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}*/
 }
